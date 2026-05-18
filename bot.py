8ef843b8b1dcc09f87eda2547af188650036fa51
Nhktech <nasirhassanknk@gmail.com>
2026-05-18 20:21:44 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telethon import TelegramClient, events
+from deep_translator import GoogleTranslator
+
+api_id = 39334379
+api_hash = "7fb7d9bd7c531ebba0b9b434c9744009"
+
+source_channel = "presstv"
+target_channel = "yaamahdi_hausa"
+
+client = TelegramClient("bot", api_id, api_hash)
+
+@client.on(events.NewMessage(chats=source_channel))
+async def handler(event):
+    msg = event.message
+    text = msg.message or ""
+
+    if text:
+        translated = GoogleTranslator(
+            source='en',
+            target='ha'
+        ).translate(text[:3000])
+
+        caption = f"📰 LABARAI\n\n{translated}\n\n📢 @yaamahdi_hausa"
+    else:
+        caption = None
+
+    if msg.media:
+        await client.send_file(target_channel, msg.media, caption=caption)
+    else:
+        await client.send_message(target_channel, caption)
+
+client.start()
+print("Bot is running...")
+client.run_until_disconnected()
